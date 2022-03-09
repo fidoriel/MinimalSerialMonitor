9d3e527c7af18bfdd953a1274b79cd765d258baf
fidoriel <49869342+fidoriel@users.noreply.github.com>
2022-03-09 20:31:26 +0100

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -295,7 +295,7 @@ void MyFrame::loadPorts()
         while( ( directory = readdir( ptr ) ) != NULL )
         {
             #if defined( __APPLE__ )
-            if ( strstr( directory->d_name, "cu.usb" ) != NULL )
+            if ( strstr( directory->d_name, "cu." ) != NULL )
             #endif
             #if defined( __linux__ ) || defined( __FreeBSD__ )
             if ( strstr( directory->d_name, "ttyUSB" ) != NULL )
